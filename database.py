461d819a8af813ff604c1ea1a06cce12cfd7abe1
Tanel Lentso <talent@ttu.ee>
2022-11-13 14:55:53 +0200

    Error handling update in database.db

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from msilib.schema import Error
 import sqlite3
 import os
 import datetime
+import tkinter.messagebox as messageBox
 
 def database(name):
     conn = connect("oldData.db")
@@ -12,8 +12,8 @@ def database(name):
 def connect(dbFile):
     try:
         connection = sqlite3.connect(dbFile)
-    except Error() as e:
-        print(e)
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
     return connection
 
 # Creates a new table in the database with raw data from file
@@ -22,8 +22,8 @@ def createTable(conn, name):
         cur = conn.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS {} (time DECIMAL, stamp DECIMAL, battery DECIMAL, pressure DECIMAL, temperature DECIMAL, ax DECIMAL, ay DECIMAL, az DECIMAL, gx DECIMAL, gy DECIMAL, gz DECIMAL, mx DECIMAL, my DECIMAL, mz DECIMAL, averagea DECIMAL);".format(name))
         conn.close()
-    except Error() as e:
-        print(e)
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
 
 # Creates a new table in the database that has all the additional data like subject name and sensor location
 def additionalDataTable(sensorId, tableName, subject, senLoc, senCon, date):
@@ -32,8 +32,8 @@ def additionalDataTable(sensorId, tableName, subject, senLoc, senCon, date):
         cur = conn.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS \"{}_data\" (\"Sensor ID\" STRING, Subject STRING, \"Sensor location\" STRING, \"Sensor condition\" STRING, \"Data created\" DATE);".format(tableName))
         conn.close()
-    except Error() as e:
-        print(e)
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
     insertIntoAddedTable(sensorId, tableName, subject, senLoc, senCon, date)
 
 def insertIntoAddedTable(sensorId, tableName, subject, senLoc, senCon, date):
@@ -43,24 +43,26 @@ def insertIntoAddedTable(sensorId, tableName, subject, senLoc, senCon, date):
         cur.execute("INSERT INTO \"{}_data\" VALUES (\"{}\", \"{}\", \"{}\", \"{}\", \"{}\");".format(tableName, sensorId, subject, senLoc, senCon, date))
         conn.commit()
         conn.close()
-    except Error() as e:
-        print(e)
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
 
 # Modifies the data inside an already existing additional data table
 def editAdditionalDataTable(sensorId, tableName, subject, senLoc, senCon, originalName):
     conn = connect("oldData.db")
-
-    cur = conn.cursor()
-    if(originalName != tableName):
-        cur.execute("ALTER TABLE \"{}_data\" RENAME TO \"{}_data\";".format(originalName, tableName))
+    try:
+        cur = conn.cursor()
+        if(originalName != tableName):
+            cur.execute("ALTER TABLE \"{}_data\" RENAME TO \"{}_data\";".format(originalName, tableName))
+            conn.commit()
+            cur.execute("ALTER TABLE \"{}\" RENAME TO \"{}\";".format(originalName, tableName))
+            conn.commit()
+            cur.execute("ALTER TABLE \"{}_peaks\" RENAME TO \"{}_peaks\";".format(originalName, tableName))
+            conn.commit()
+        cur.execute("UPDATE \"{}_data\" SET \"Sensor ID\"=\"{}\", Subject=\"{}\", \"Sensor location\"=\"{}\", \"Sensor condition\"=\"{}\";".format(tableName, sensorId, subject, senLoc, senCon))
         conn.commit()
-        cur.execute("ALTER TABLE \"{}\" RENAME TO \"{}\";".format(originalName, tableName))
-        conn.commit()
-        cur.execute("ALTER TABLE \"{}_peaks\" RENAME TO \"{}_peaks\";".format(originalName, tableName))
-        conn.commit()
-    cur.execute("UPDATE \"{}_data\" SET \"Sensor ID\"=\"{}\", Subject=\"{}\", \"Sensor location\"=\"{}\", \"Sensor condition\"=\"{}\";".format(tableName, sensorId, subject, senLoc, senCon))
-    conn.commit()
-    conn.close()
+        conn.close()
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
 
 def deleteAllSelectedData(tableName):
     conn = connect("oldData.db")
@@ -81,8 +83,8 @@ def createPeaks(name):
         cur.execute("DROP TABLE IF EXISTS {};".format(tableName))
         cur.execute("CREATE TABLE IF NOT EXISTS {} (peak DECIMAL);".format(tableName))
         conn.close()
-    except Error as e:
-        print(e)
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
 
 
 
@@ -94,8 +96,8 @@ def insertPeaks(tableName, peak):
         cur.execute("INSERT INTO \"{}_peaks\" VALUES (\"{}\");".format(tableName, peak))
         conn.commit()
         conn.close()
-    except Error as e:
-        print(e)
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
 
 def returnPeaks(item):
     conn = connect("oldData.db")
@@ -109,5 +111,5 @@ def returnPeaks(item):
         conn.close()
         return peaksList
 
-    except Error as e:
-        print(e)
+    except sqlite3.Error as e:
+        messageBox.showerror("Error", e)
